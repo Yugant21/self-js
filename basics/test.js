e8e233a0cf7abe384cebdaad43e53b4f8f1edfa0
Yugant21 <96230417+Yugant21@users.noreply.github.com>
2024-01-08 16:04:49 +0000

    new commit

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,10 @@
-console.log("yugant")
+let arr_1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; // --> 276
+let arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26];
+
+for (let i = 1; i <= 10; i++) {
+    // const element = arr_1[i];
+    // console.log(i)
+    for (let j = 1; j <= 10; j++) {
+        // console.log(i + '*' + j + '=' + i*j)
+    }
+}
